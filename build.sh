26712a8901dda341c6962b29051dbc84b334b922
Sagarsuman07 <sagarsumanrathore@gmail.com>
2025-03-16 06:05:38 +0530

    try different build file

--- build.sh
+++ build.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
+set -e
+set -x
+
 echo "Checking Java installation"
-java -version || { echo "Java not found, please install Java"; exit 1; }
+java -version || { echo "Java not found in Docker container, which is unexpected"; exit 1; }
 
 echo "Setting up JAVA_HOME"
-# More reliable way to find JAVA_HOME
-if [ -z "$JAVA_HOME" ]; then
-  java_path=$(readlink -f $(which java))
-  # Remove the /bin/java part
-  export JAVA_HOME=${java_path%/bin/java}
-  # Alternative approach if the above doesn't work
-  # export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
-fi
-
+export JAVA_HOME=$(dirname $(dirname $(readlink -f $(which java))))
 echo "JAVA_HOME set to: $JAVA_HOME"
+
 echo "Making Maven wrapper executable"
 chmod +x ./mvnw
+
 echo "Running Maven build"
 ./mvnw clean package -DskipTests
